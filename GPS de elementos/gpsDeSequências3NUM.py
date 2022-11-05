e5aaed86af2b63e13d38ebcdd45e87e4d4782bdc
Rafael Brant <117216968+RafaelBrantPr@users.noreply.github.com>
2022-11-05 18:42:54 -0300

    Add files via upload

---
+++ GPS de elementos/gpsDeSequências3NUM.py
@@ -0,0 +1,17 @@
+#Calculadora de sequências com padrão. 3 elementos
+
+tam_seq = 3
+seq1 = int(input('digite o 1º elemento da seq'))
+seq2 = int(input('digite o 2º elemento da seq'))
+seq3 = int(input('digite o 3º elemento da seq'))
+calc = int(input('Digite a posição do elemento'))
+position = calc%tam_seq
+
+if position == 0:
+    position = seq3
+elif position == 1:
+    position = seq1
+else:
+    position = seq2
+ 
+print(f"O elemento na {calc}º posição é: {position}")
